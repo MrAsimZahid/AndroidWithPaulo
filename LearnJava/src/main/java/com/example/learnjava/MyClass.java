07c4295bf594474924cd80fd5d7ae1112ef09e21
Asim Zahid <asimzahid02@gmail.com>
2020-02-08 12:31:40 +0500

    Arithmatic operations in java and type casting.

--- LearnJava/src/main/java/com/example/learnjava/MyClass.java
+++ LearnJava/src/main/java/com/example/learnjava/MyClass.java
@@ -4,40 +4,46 @@ public class MyClass {
 
     public static void main(String[] args) {
 
-        String name = "Asim";
-        String intro = "How are you doing?";
-        //sub-set of string
-        char chr = 'A';         //can hold only  one character
+        int a = 2;
+        double b = 2;
 
+        int total = (int) (a+b); // casted double to int datatype
+        System.out.println(total);
 
-        int age = 23;
-        //long dataType
-        long weightOfPlanet = 994333888;
-        byte byt = 1;           // Range= (-127 to 127)
-        short shot = 200;       // 2x byte
-
-
-
-
-        //float dataType to deal with decimal numbers
-        float pi = 3.14f;
-        //double dataType
-        // only difference between float and double is memory size and declaration style.
-        double Pi = 3.14; // for more precision
-
-        //Boloaen dataType
-        boolean leaarner = true;
-
-
-        // Print statements
-        System.out.println("Hello World!");
-        System.out.println("I am a programmer");
-        System.out.println(name);
-        System.out.println(intro);
-        System.out.println("My age is : " + age);
-        System.out.println("My name is " + name + ". I am " + age + " years old. How old are you?");
-        System.out.println("The value of Pi is " + pi);
-
+//        String name = "Asim";
+//        String intro = "How are you doing?";
+//        //sub-set of string
+//        char chr = 'A';         //can hold only  one character
+//
+//
+//        int age = 23;
+//        //long dataType
+//        long weightOfPlanet = 994333888;
+//        byte byt = 1;           // Range= (-127 to 127)
+//        short shot = 200;       // 2x byte
+//
+//
+//
+//
+//        //float dataType to deal with decimal numbers
+//        float pi = 3.14f;
+//        //double dataType
+//        // only difference between float and double is memory size and declaration style.
+//        double Pi = 3.14; // for more precision
+//
+//        //Boloaen dataType
+//        boolean leaarner = true;
+//
+//
+//        // Print statements
+//        System.out.println("Hello World!");
+//        System.out.println("I am a programmer");
+//        System.out.println(name);
+//        System.out.println(intro);
+//        System.out.println("My age is : " + age);
+//        System.out.println("My name is " + name + ". I am " + age + " years old. How old are you?");
+//        System.out.println("The value of Pi is " + pi);
+//
     }
 
 }
