a81e0f55becd3cf9c11586de9a5e171d6c257718
Asim Zahid <asimzahid02@gmail.com>
2020-02-08 11:35:58 +0500

    Playing with strings and variables.

--- LearnJava/src/main/java/com/example/learnjava/MyClass.java
+++ LearnJava/src/main/java/com/example/learnjava/MyClass.java
@@ -3,7 +3,15 @@ package com.example.learnjava;
 public class MyClass {
 
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+
+        String name = "Hey! Asim is here.";
+        String intro = "How are you doing?";
+
+        System.out.println("Hello World!");
+        System.out.println("I am a programmer");
+        System.out.println(name);
+        System.out.println(intro);
+
     }
 
 }
