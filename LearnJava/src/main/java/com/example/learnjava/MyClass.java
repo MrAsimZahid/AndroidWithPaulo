c66c363a79dda96e121ce22a09186dfa9785a340
Asim Zahid <asimzahid02@gmail.com>
2020-02-08 11:48:31 +0500

    Playing with concatinations and integers.

--- LearnJava/src/main/java/com/example/learnjava/MyClass.java
+++ LearnJava/src/main/java/com/example/learnjava/MyClass.java
@@ -4,13 +4,17 @@ public class MyClass {
 
     public static void main(String[] args) {
 
-        String name = "Hey! Asim is here.";
+        String name = "Asim";
         String intro = "How are you doing?";
+        int age = 23;
+
 
         System.out.println("Hello World!");
         System.out.println("I am a programmer");
         System.out.println(name);
         System.out.println(intro);
+        System.out.println("My age is : " + age);
+        System.out.println("My name is " + name + ". I am " + age + " years old. How old are you?");
 
     }
 
